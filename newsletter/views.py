0e454cd5874713557a0e7fce379629cc6f599511
BaronBenny <baronbenny23@gmail.com>
2020-10-14 23:39:35 +0000

    Done with the first realease of the website tho

---
+++ newsletter/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Newsletter
+from django.views.decorators.http import require_POST
+from .forms import NewsletterForm
+# Create your views here.
+@require_POST
+def subscribe(request):
+	if request.method == 'POST':
+		print(request.POST)
+		form = NewsletterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('about')
+		else:
+			form = NewsletterForm()
+			return redirect('home')
+
+	return render(request, 'index.html', {'Form':form})
